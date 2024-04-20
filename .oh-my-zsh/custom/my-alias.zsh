88a656688acef46c8dbbe968ab872c3bddcf1961
Jon Kamke <jon@kamke.org>
2024-04-20 16:12:14 -0500

    feat: alias changes

--- .oh-my-zsh/custom/my-alias.zsh
+++ .oh-my-zsh/custom/my-alias.zsh
@@ -1,4 +1,8 @@
 # custom aliases
+
+# Bare bones Git command
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+# ZSH
 alias zshconfig="vim ~/.zshrc"
 alias zs="source ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
